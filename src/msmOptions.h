a6b529044db77135b42625b2ada5ad2b6be9ba59
Renátó Besenczi <renato.besenczi@gmail.com>
2023-04-24 17:38:03 +0100

    Working on params...

--- src/msmOptions.h
+++ src/msmOptions.h
@@ -1,15 +1,7 @@
 #ifndef MSMOPTIONS_H
 #define MSMOPTIONS_H
 
-#include <string>
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <cstdio>
-
 #include "utils/options.h"
-#include "utils/log.h"
 
 class msmOptions {
 
@@ -29,16 +21,13 @@ public:
     Utilities::Option<std::string> CMmatrixin;
     Utilities::Option<std::string> CMmatrixref;
     Utilities::Option<std::string> transformed_sphere;
-    Utilities::Option<std::string> in_register;
     Utilities::Option<std::string> cfweight_in;
     Utilities::Option<std::string> cfweight_ref;
     Utilities::Option<std::string> outbase;
     Utilities::Option<std::string> outformat;
     Utilities::Option<std::string> parameters;
-    Utilities::Option<int> multiresolutionlevels;
-    Utilities::Option<float> smoothoutput;
 
-    bool parse_command_line(int argc, char **argv, Utilities::Log &logger);
+    bool parse_command_line(int argc, char **argv);
 
     const msmOptions& operator=(const msmOptions&) = delete;
     msmOptions(const msmOptions&) = delete;
@@ -61,6 +50,7 @@ inline msmOptions& msmOptions::getInstance() {
 }
 
 inline msmOptions::msmOptions() :
+        options("msm", "msm [options]\n"),
         help(std::string("-h,--help"), false,
              std::string("display this message"),
              false, Utilities::no_argument),
@@ -94,9 +84,6 @@ inline msmOptions::msmOptions() :
         transformed_sphere(std::string("--trans"), std::string(""),
                            std::string("\t Transformed source mesh (output of a previous registration). Use this to initiliase the current registration."),
                            false , Utilities::requires_argument),
-        in_register(std::string("--in_register"), std::string(""),
-                    std::string("\t Input mesh at data resolution. Used to resample data onto input mesh if data is supplied at a different resolution. Note this mesh HAS to be in alignment with either the input_mesh of (if supplied) the transformed source mesh. Use with supreme caution."),
-                    false , Utilities::requires_argument),
         cfweight_in(std::string("--inweight"), std::string(""),
                     std::string("cost function weighting for input - weights data in these vertices when calculating similarity (ASCII or GIFTI). Can be multivariate provided dimension equals that of data "),
                     false , Utilities::requires_argument),
@@ -111,14 +98,7 @@ inline msmOptions::msmOptions() :
                   false, Utilities::requires_argument),
         parameters(std::string("--conf"), std::string(""),
                    std::string("\tconfiguration file "),
-                   false, Utilities::requires_argument),
-        multiresolutionlevels(std::string("--levels"),0,
-                        std::string("number of resolution levels (default = number of resolution levels specified by --opt in config file)"),
-                              false, Utilities::requires_argument),
-        smoothoutput(std::string("--smoothout"), 0,
-                     std::string("smooth tranformed output with this sigma (default=0)"),
-                     false, Utilities::requires_argument),
-        options("msm", "msm [options]\n")
+                   false, Utilities::requires_argument)
     {
     try {
         options.add(help);
@@ -132,14 +112,11 @@ inline msmOptions::msmOptions() :
         options.add(CMmatrixin);
         options.add(CMmatrixref);
         options.add(transformed_sphere);
-        options.add(in_register);
         options.add(cfweight_in);
         options.add(cfweight_ref);
         options.add(outbase);
         options.add(outformat);
         options.add(parameters);
-        options.add(multiresolutionlevels);
-        options.add(smoothoutput);
     }
     catch(Utilities::X_OptionError& e)
     {
@@ -152,7 +129,7 @@ inline msmOptions::msmOptions() :
     }
 }
 
-inline bool msmOptions::parse_command_line(int argc, char **argv, Utilities::Log &logger) {
+inline bool msmOptions::parse_command_line(int argc, char **argv) {
 
     for (int a = options.parse_command_line(argc, argv); a < argc; a++);
     if (!(printoptions.value()))
@@ -162,14 +139,6 @@ inline bool msmOptions::parse_command_line(int argc, char **argv, Utilities::Log
             options.usage();
             exit(2);
         }
-
-        logger.makeDir(outbase.value() + "logdir", "MSM.log");
-
-        // do again so that options are logged
-        for (int a = 0; a < argc; a++)
-            logger.str() << argv[a] << " ";
-
-        logger.str() << std::endl << "---------------------------------------------" << std::endl << std::endl;
     }
     return true;
 }
--- src/newmsm.cpp
+++ src/newmsm.cpp
@@ -5,16 +5,13 @@ msmOptions *msmOptions::gopt = nullptr;
 
 int main(int argc, char *argv[]) {
 
-    std::cout << "This is newMSM v0.2.0-BETA." << std::endl;
+    std::cout << "This is newMSM v0.3.0-BETA." << std::endl;
 
     msmOptions &opts = msmOptions::getInstance();
-    Utilities::Log &logger = Utilities::LogSingleton::getInstance();
-    std::string CMpathin;
+    opts.parse_command_line(argc, argv);
 
-    opts.parse_command_line(argc, argv, logger);
     newmeshreg::Mesh_registration MR;
 
-    //---INITIALISE VARIABLES---//
     if (opts.printoptions.value())
     {
         MR.print_config_options();
@@ -25,10 +22,10 @@ int main(int argc, char *argv[]) {
     if (opts.debug.value()) MR.set_debug(opts.debug.value());
 
     MR.set_input(opts.inputmesh.value());
+
     if (opts.referencemesh.value().empty()) MR.set_reference(opts.inputmesh.value());
     else MR.set_reference(opts.referencemesh.value());
 
-    // add anatomical meshes for stress and strain
     if (!opts.inputanatmesh.value().empty())
     {
         if (opts.referenceanatmesh.value().empty())
@@ -46,52 +43,10 @@ int main(int argc, char *argv[]) {
     if (opts.cfweight_in.set()) MR.set_input_cfweighting(opts.cfweight_in.value());
     if (opts.cfweight_ref.set()) MR.set_reference_cfweighting(opts.cfweight_ref.value());
 
-    if (opts.in_register.set())
-    {
-        // if data is supplied at a different resolution to the input or
-        // transformed mesh it is necessary to resample i.e. HCP 32K to native
-        // the in_register sphere HAS to be the sphere on which the data
-        // is supplied and the input or transformed mesh MUST be in alignment with it.
-        newresampler::Mesh in_register, target;
-
-        if (opts.transformed_sphere.set()) target.load(opts.transformed_sphere.value());
-        else target.load(opts.inputmesh.value());
-        in_register.load(opts.in_register.value());
-
-        std::shared_ptr<MISCMATHS::BFMatrix> DATA;
-        newmeshreg::set_data(opts.CMmatrixin.value(), DATA, in_register);
-
-        in_register.set_pvalues(DATA->AsMatrix());
-        in_register = newresampler::metric_resample(in_register, target);
-        target.set_pvalues(DATA->AsMatrix());
-
-        std::string fname = opts.outbase.value() + "input_in_register.func.gii";
-        target.save(fname);
-        CMpathin = fname;
-    }
-    else
-        CMpathin = opts.CMmatrixin.value();
-
-    MR.set_CMpathin(CMpathin);
+    MR.set_CMpathin(opts.CMmatrixin.value());
     MR.set_CMpathref(opts.CMmatrixref.value());
 
-    MR.run_multiresolutions(opts.multiresolutionlevels.value(), opts.smoothoutput.value(), opts.parameters.value());
-
-    if (opts.in_register.set())
-    {
-        // if data is supplied at a lower mesh resolution,
-        // then resample final warp accordingly.
-        // This is typically a HCP formating issue.
-        newresampler::Mesh ORIG, FINAL, in_register;
-
-        if (opts.transformed_sphere.set()) ORIG.load(opts.transformed_sphere.value());
-        else ORIG.load(opts.inputmesh.value());
-
-        in_register.load(opts.in_register.value());
-        FINAL.load(opts.outbase.value() + "sphere.reg" + MR.get_surf_format());
-        in_register = newresampler::surface_resample(in_register, ORIG, FINAL);
-        in_register.save(opts.outbase.value() + "sphere.in_register.reg." + MR.get_surf_format());
-    }
+    MR.run_multiresolutions(opts.parameters.value());
 
     return 0;
 }
