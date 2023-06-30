0cdd44d1869f47a9d678254217e0e37f6e1a66be
Renato Besenczi <renato.besenczi@gmail.com>
2023-06-30 16:35:08 +0100

    New bugfix version.

--- src/newmsm.cpp
+++ src/newmsm.cpp
@@ -5,7 +5,7 @@ msmOptions *msmOptions::gopt = nullptr;
 
 int main(int argc, char *argv[]) {
 
-    std::cout << "This is newMSM v0.3.1-BETA." << std::endl;
+    std::cout << "This is newMSM v0.4.1-BETA." << std::endl;
 
     msmOptions &opts = msmOptions::getInstance();
     opts.parse_command_line(argc, argv);
