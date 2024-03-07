61feba131b14d7baff7b2de7093644e033951458
Renato Besenczi <renato.besenczi@gmail.com>
2024-03-07 13:26:19 +0000

    Reorganisation of HCP registration script.

--- gMSM_scripts/newMSM_HCP_to_template.sh
+++ gMSM_scripts/newMSM_HCP_to_template.sh
@@ -1,43 +1,67 @@
-#!/bin/bash -l
+#!/bin/bash 
 
 ###########################################################
-## The following few lines are for running on CREATE cluster. Ignore it otherwise.
+## Set the following lines according to your settings and cohort.
+nthreads=8
+parallel_tasks=$(( 350/$nthreads ))
+
+dataset="HCP"
+workdir=$HOME/${dataset}_to_template
+input_folder=$HOME/affined_${dataset}_sulc
+template=$workdir/MSMSulc.L.sulc.ico6.shape.gii
+mesh=$workdir/sunet.ico-6.sphere.surf.gii
+config_file=$workdir/HCP_to_template_config.txt
+sublist=$workdir/subjects_in_study.txt
+outdir=$workdir/output
+###########################################################
+
+num_subjects=$(cat $sublist | wc -l)
+bunch_size=$(( $num_subjects / $parallel_tasks + 1 ))
+
+touch $workdir/newMSM_HCP_to_template.sh
+mkdir $outdir
+mkdir $workdir/logs
+
+for (( bunch=0; bunch<parallel_tasks; bunch++ ))
+do
+  echo "#!/bin/bash -l
+
+###########################################################
 #SBATCH --partition=cpu
 #SBATCH --time=0-12:00
 #SBATCH --nodes=1
 #SBATCH --mem=8192
-#SBATCH --ntasks=8
-#SBATCH --job-name=newMSM
-#SBATCH --output=./newMSM_HCP_to_tmp_subs_NODE2214_39.txt
-## how to run on create: $ sbatch newMSM_HCP_to_template.sh
-###########################################################
+#SBATCH --ntasks=$nthreads
+#SBATCH --job-name=MSM
+#SBATCH --output=${workdir}/logs/HCP_to_template_bunch_${bunch}.txt
+###########################################################" > $workdir/newMSM_HCP_to_template.sh
 
-###########################################################
-## Set the following lines according to your settings and cohort.
-workdir=$HOME/newMSM_HCP_to_tmp
-input_folder=/scratch/prj/cortical_imaging/Renato/affined_sulc
-template=/scratch/prj/cortical_imaging/Renato/groupwise/HCP/results/NODE2214_HPC/groupwise.NODE2214_HPC.mean.sulc.affine.dedrifted.ico6.shape.gii
-mesh=$workdir/template/sunet.ico-6.template.surf.gii
-config_file=$workdir/configs/newMSM_HCP_to_template_config_NODE2214.txt
-sublist=/scratch/prj/cortical_imaging/Renato/groupwise/HCP/group_lists/NODE2214_HPC.csv
-outdir=/scratch/prj/cortical_imaging/Renato/HCP_to_template/2
-bunch=39
-###########################################################
+  from=$(( bunch * bunch_size + 1 ))
+  to=$(( from + bunch_size - 1 ))
 
-from=$(( bunch * 20 + 1 ))
-to=$(( from + 19 ))
-
+  echo "
 while IFS=',' read -r subject group
 do
-  echo "Registering subject $subject to the template..."
+  echo \"Registering subject \$subject to the template...\"
 
-  time $HOME/fsldev/bin/newmsm \
-  --inmesh=$mesh \
-  --refmesh=$mesh \
-  --indata="$input_folder/$subject.sulc.affine.ico6.shape.gii" \
-  --refdata=$template \
-  --conf=$config_file \
-  --out=$outdir/$subject.MSMSulc.ico6. \
+  time $HOME/fsldev/bin/newmsm \\
+  --inmesh=$mesh \\
+  --refmesh=$mesh \\
+  --indata="$input_folder/\$subject.sulc.affine.ico6.shape.gii" \\
+  --refdata=$template \\
+  --conf=$config_file \\
+  --out=$outdir/\$subject.MSMSulc.ico6. \\
   --verbose
 
-done < <(sed -n "${from},${to}p" $sublist)
+done < <(sed -n \"${from},${to}p\" $sublist)
+  " >> $workdir/newMSM_HCP_to_template.sh
+
+  echo "Submitting job #$(( $bunch + 1 )) to slurm."
+  sbatch $workdir/newMSM_HCP_to_template.sh
+
+  if [ $? -ne 0 ];
+  then
+    exit 1
+  fi
+
+done
